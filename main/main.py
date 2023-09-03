843e3ec607a49bebb4571d691d2a979d60fc8396
haminhesam <hesamhajati@gmail.com>
2023-09-03 10:07:52 +0330

    Version Initial 1.0

---
+++ main/main.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# Set up the Selenium WebDriver for your preferred browser
+driver = webdriver.Firefox()  # You can use other browsers by changing this line
+
+# Navigate to the web page
+url = 'https://divar.ir/s/iran?q=%D8%AA%D8%B1%D8%A7%D8%B3%D8%AA'  # Replace with the URL of the page you want to scrape
+driver.get(url)
+
+# Define a function to scroll down and load more results
+def scroll_to_bottom():
+    # Scroll down to the bottom of the page using JavaScript
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+page_height = driver.execute_script("return document.body.scrollHeight")
+
+# Scroll down and wait for more results until there are no more
+while True:
+    # Scroll down to the bottom of the page
+    scroll_to_bottom()
+    
+    # Wait for a few seconds to allow new content to load (customize the sleep time)
+    time.sleep(3)  # Adjust this delay as necessary
+    
+    # Get the new page height
+    new_page_height = driver.execute_script("return document.body.scrollHeight")
+    
+    # Check if the page height has not changed, indicating the end of loading
+    if new_page_height == page_height:
+        break
+    
+    # Update the page height
+    page_height = new_page_height
+
+# Extract article links
+article_links = []
+article_elements = driver.find_elements(By.XPATH, '//a[@class="article-link"]')  # Customize the XPath
+for element in article_elements:
+    article_links.append(element.get_attribute('href'))
+    print (element.text)
+# You now have the article links in the 'article_links' list
+print(article_links)
+
+# Once done, you can perform further processing on the extracted links as needed
+
+# Close the WebDriver when done
+driver.quit()
